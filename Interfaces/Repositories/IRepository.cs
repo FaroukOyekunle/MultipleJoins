873b2a9e17655c16e25501af66bccb7a02c53c1c
Fārūqī al-Nūrī <94355638+FaroukOyekunle@users.noreply.github.com>
2025-01-06 06:33:27 +0100

    Add project files.

--- Interfaces/Repositories/IRepository.cs
+++ Interfaces/Repositories/IRepository.cs
@@ -1,16 +1,16 @@
 ﻿using MongoDB.Bson;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace MultipleJoins.Interfaces.Repositories
 {
     public interface IRepository<T> where T : class
     {
+        Task AddAsync(T entity);
+        Task DeleteAsync(ObjectId id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(ObjectId id);
-        Task AddAsync(T entity);
         Task UpdateAsync(ObjectId id, T entity);
-        Task DeleteAsync(ObjectId id);
         Task<IEnumerable<dynamic>> JoinMultipleCollectionsAsync(dynamic payload);
     }
 }
